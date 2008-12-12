5aa8c71d788b8350791aafb67f1c05ed6a25b45f
Stephan Zuercher <szuercher@lucidera.com>
2008-12-11 22:37:52 -0800

    ENKI: remove redundant header and fix grammar
    [git-p4: depot-paths = "//open/enki/": change = 12102]

--- src/org/eigenbase/enki/test/performance/BackupRestoreLargeDataSetTest.java
+++ src/org/eigenbase/enki/test/performance/BackupRestoreLargeDataSetTest.java
@@ -19,27 +19,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
-/*
-// $Id$
-// Enki generates and implements the JMI and MDR APIs for MOF metamodels.
-// Copyright (C) 2008-2008 The Eigenbase Project
-// Copyright (C) 2008-2008 Disruptive Tech
-// Copyright (C) 2008-2008 LucidEra, Inc.
-//
-// This library is free software; you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation; either version 2.1 of the License, or (at
-// your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-*/
 package org.eigenbase.enki.test.performance;
 
 import java.io.*;
@@ -432,7 +411,7 @@ public class BackupRestoreLargeDataSetTest extends SampleModelTestBase
                 "BackupRestoreLargeDataSet.xmi");
         if (importSource == null) {
             Assert.fail(
-                "Execute this class's main() method to generate an XMI large data set XMI file");
+                "Execute this class's main() method to generate a large data set XMI file");
         }
         File backup = 
             new File("test/results/BackupRestoreLargeDataSetTest.backup");
